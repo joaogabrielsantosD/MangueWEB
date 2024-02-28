1166d708c9264a6ed9bbc9efc19e81a99481f477
pedro <pedro.adoneto@gmail.com>
2024-02-28 16:14:00 -0300

    routing done

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mangueweb/screens/backup_screen.dart';
 import 'package:mangueweb/screens/home_screen.dart';
+import 'package:mangueweb/screens/live_screen.dart';
 
 class RouteGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case 'live':
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
+        return MaterialPageRoute(builder: (_) => const LiveScreen());
       case 'backup':
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
+        return MaterialPageRoute(builder: (_) => const BackupScreen());
       default:
         return MaterialPageRoute(
             builder: (_) => const HomeScreen()); // return to init screen
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,115 +6,127 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-          gradient: LinearGradient(
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter,
-        colors: [
-          Color.fromRGBO(1, 176, 48, 1),
-          Color.fromRGBO(0, 17, 152, 1),
-        ],
-      )),
-      child: CustomPaint(
-        painter: SplineDividerPainter(),
-        size: Size.infinite,
-        child: Row(
-          children: [
-            Expanded(
-              flex: 1, // Takes 75% of the space
-              child: Container(), // Empty container just to take up space
-            ),
-            Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 200,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Image.asset(
-                      'images/home_logo.png',
-                      fit: BoxFit.fitWidth,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40),
-                          gradient: const LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              Color.fromRGBO(5, 112, 34, 1),
-                              Color.fromRGBO(57, 180, 90, 1),
-                            ],
-                          ),
-                        ),
-                        width: 200,
-                        height: 51,
-                        child: const Center(
-                          child: DefaultTextStyle(
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              fontFamily:
-                                  'Roboto', // Ensure the font is available or specified in pubspec.yaml
-                              color:
-                                  Colors.white, // Optional: Adjust text color
-                            ),
-                            child: Text(
-                              "Live",
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(
-                        width: 12,
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(40),
-                          gradient: const LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [
-                              Color.fromRGBO(5, 112, 34, 1),
-                              Color.fromRGBO(57, 180, 90, 1),
-                            ],
-                          ),
-                        ),
-                        width: 200,
-                        height: 51,
-                        child: const Center(
-                          child: DefaultTextStyle(
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              fontFamily:
-                                  'Roboto', // Ensure the font is available or specified in pubspec.yaml
-                              color:
-                                  Colors.white, // Optional: Adjust text color
-                            ),
-                            child: Text(
-                              "Backup",
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
+    return Material(
+      child: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromRGBO(1, 176, 48, 1),
+            Color.fromRGBO(0, 17, 152, 1),
           ],
+        )),
+        child: CustomPaint(
+          painter: SplineDividerPainter(),
+          size: Size.infinite,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1, // Takes 75% of the space
+                child: Container(), // Empty container just to take up space
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Image.asset(
+                        'images/home_logo.png',
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(context, 'live');
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  Color.fromRGBO(5, 112, 34, 1),
+                                  Color.fromRGBO(57, 180, 90, 1),
+                                ],
+                              ),
+                            ),
+                            width: 200,
+                            height: 51,
+                            child: const Center(
+                              child: DefaultTextStyle(
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily:
+                                      'Roboto', // Ensure the font is available or specified in pubspec.yaml
+                                  color: Colors
+                                      .white, // Optional: Adjust text color
+                                ),
+                                child: Text(
+                                  "Live",
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 12,
+                        ),
+                        InkWell(
+                          onTap: () {
+                            Navigator.pushNamed(context, 'backup');
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(40),
+                              gradient: const LinearGradient(
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                                colors: [
+                                  Color.fromRGBO(5, 112, 34, 1),
+                                  Color.fromRGBO(57, 180, 90, 1),
+                                ],
+                              ),
+                            ),
+                            width: 200,
+                            height: 51,
+                            child: const Center(
+                              child: DefaultTextStyle(
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily:
+                                      'Roboto', // Ensure the font is available or specified in pubspec.yaml
+                                  color: Colors
+                                      .white, // Optional: Adjust text color
+                                ),
+                                child: Text(
+                                  "Backup",
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
